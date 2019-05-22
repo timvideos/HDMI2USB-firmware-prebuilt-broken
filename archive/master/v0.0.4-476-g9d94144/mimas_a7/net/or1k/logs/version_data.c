1ef160b444cef86ee95f098840f247541ad630ee
TimVideos.us Robot <robot@timvideos.us>
2019-05-21 20:15:43 -0500

    Travis build #1158 of master/v0.0.4-476-g9d94144 for PLATFORM=mimas_a7 TARGET=net CPU=or1k FIRMWARE=firmware
    From https://github.com/timvideos/HDMI2USB-litex-firmware/tree/9d9414468bb3eddf08bde12c2b27db5e75ff7b50

---
+++ archive/master/v0.0.4-476-g9d94144/mimas_a7/net/or1k/logs/version_data.c
@@ -0,0 +1,19 @@
+
+#ifndef PLATFORM_MIMAS_A7
+#error "Version mismatch - PLATFORM_MIMAS_A7 not defined!"
+#endif
+const char* board = "mimas_a7";
+
+#ifndef TARGET_NET
+#error "Version mismatch - TARGET_NET not defined!"
+#endif
+const char* target = "net";
+
+const char* git_commit = "9d9414468bb3eddf08bde12c2b27db5e75ff7b50";
+const char* git_branch = "master";
+const char* git_describe = "v0.0.4-476-g9d94144";
+const char* git_status =
+    "    --\r\n"
+
+    "    --\r\n";
+
